15bfe4e1a24faa6f4768576e32ba3372bc71a6ce
Mabala-Karabo <mabalakarabo25@gmail.com>
2023-09-07 21:06:53 +0200

    Add project files.

---
+++ Passing1/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim string1 As New Form2 'instance of a class form2
+        string1.StringPass = TextBox1.Text 'assigning string variable which was declared in form2 variable 
+        string1.Show() ' showing form2
+    End Sub
+End Class
---
+++ Passing1/Form2.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form2
+    'declaring puplic variable named StringPass
+    Public Property StringPass As String
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = StringPass 'label1 from form2 
+    End Sub
+End Class
